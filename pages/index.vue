f13093511e407aeace07c240e13b9ba2f37ca4e8
yanase-kenta0505 <ynsknt0505@gmail.com>
2022-03-16 22:58:52 +0900

    unplugin-vue2-script-setupをインストール

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
+
 <script setup lang="ts">
+
+const title = 'TODO LIST'
 
 </script>
 
-
 <template>
-<v-app>
-  <h1>aaaa</h1>
-  <v-btn></v-btn>
-</v-app>
+  <v-app>
+    <h1 class="ml-5 mt-5 red">{{title}}</h1>
+  </v-app>
 </template>
 
